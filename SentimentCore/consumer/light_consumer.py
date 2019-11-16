aecdc236a8de64425c9efd3f89ed68a33c64dbb4
Martin Stypinski <mstypinski@gmail.com>
2019-11-16 20:24:57 +0100

    Added demo with keyboard

--- SentimentCore/consumer/light_consumer.py
+++ SentimentCore/consumer/light_consumer.py
@@ -1,7 +1,31 @@
+import colorsys
+import json
+
+import requests
+
 from SentimentCore.core.observer import Observer
+from SentimentCore.util.color_space_utils import calculate_color
+from SentimentCore.util.rgb_xy_converter import Converter
 
 
 class LightConsumer(Observer):
 
-    def update(self, subject):
-        print("Emotion is " + str(subject))
+    def __init__(self):
+        self._token = "U5n6uDzCWE0dyMGOU3auhvRKS7WaxCYiWHeMIAxx"
+        self._light_ids = [1, 2]
+        self._angry_color = (255, 0, 0)
+        self._happy_color = (0, 255, 0)
+        self._converter = Converter()
+
+    def update(self, emotional_score):
+        r, g, b = calculate_color(self._angry_color, self._happy_color, emotional_score)
+        xy = self._converter.rgb_to_xy(r, g, b)
+        state = False
+
+        for light_id in self._light_ids:
+            URL = "http://192.168.1.2/api/{}/lights/{}/state".format(self._token, light_id)
+            BODY = {"on": state, "xy": xy}
+            BODY = json.dumps(BODY)
+
+            requests.put(url=URL, data=BODY)
+
