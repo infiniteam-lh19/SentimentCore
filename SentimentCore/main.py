22d1feb7bf06dc4c4caba81b8df0e4520a69eede
Kirusanth Poopalasingam <code@kiru.io>
2019-11-17 00:18:16 +0100

    WebSocket

--- SentimentCore/main.py
+++ SentimentCore/main.py
@@ -3,8 +3,8 @@ from time import sleep
 
 from SentimentCore.consumer.keyboard_consumer import KeyboardConsumer
 from SentimentCore.consumer.light_consumer import LightConsumer
+from SentimentCore.consumer.websocket_consumer import WebSocketConsumer
 from SentimentCore.core.state_change_dispatcher import StateChangeDispatcher
-
 
 def main():
     print("Test")
@@ -12,9 +12,11 @@ def main():
     dispatcher = StateChangeDispatcher()
     keyboardConsumer = KeyboardConsumer()
     lightConsumer = LightConsumer()
+    websocketConsumer = WebSocketConsumer()
 
     dispatcher.attach(lightConsumer)
     dispatcher.attach(keyboardConsumer)
+    dispatcher.attach(websocketConsumer)
 
     while True:
         dispatcher.event_update(random.random())
